4447bac5b90fa3c21baace72c89d2aa952a9523c
Yuri Resende <yuuriresende@hotmail.com>
2022-01-07 19:35:41 -0300

    inicializacao do projeto com pagina home e novas salas

---
+++ src/pages/Home.tsx
@@ -0,0 +1,40 @@
+import illustraionImg from '../assets/images/illustration.svg'
+import logoImg from '../assets/images/logo.svg'
+import googleIconImg from '../assets/images/google-icon.svg'
+
+import '../styles/auth.scss'
+import { Button } from '../components/Button'
+
+export function Home(){
+    return(
+        <div id="page-auth">
+            <aside>
+                <img src={illustraionImg} alt="ilustracao simbolizando perguntas e respostas" />
+                <strong>Crie salas de Q&A ao-vivo</strong>
+                <p>tire suas duvidas da audiencia em tempo real</p>
+            </aside>
+            <main>
+                <div className="main-content">
+                    <img src={logoImg} alt="logo" />
+                
+                    <button className="create-room">
+                        <img src={googleIconImg} alt="imagem do google" />
+                        crie sua sala com o Google
+                    </button>
+                    <div className="separator">ou entre em uma sala</div>
+                    <form>
+                        <input 
+                        type="text"
+                        placeholder="digite o codigo de sala" 
+                        />
+                        <Button type="submit">
+                        Entrar na sala
+                    </Button>
+                    </form>
+                    <p>Quer entrar em uma sala existete? <a href="#">clique aqui</a></p>
+                </div>
+            </main>
+        </div>
+    )
+
+}
---
+++ src/pages/NewRoom.tsx
@@ -0,0 +1,33 @@
+import '../styles/auth.scss'
+import illustraionImg from '../assets/images/illustration.svg'
+import logoImg from '../assets/images/logo.svg'
+import { Button } from '../components/Button'
+
+export function NewRoom(){
+    return(
+        <div id="page-auth">
+            <aside>
+                <img src={illustraionImg} alt="ilustracao simbolizando perguntas e respostas" />
+                <strong>Crie salas de Q&A ao-vivo</strong>
+                <p>tire suas duvidas da audiencia em tempo real</p>
+            </aside>
+            <main>
+                <div className="main-content">
+                    <img src={logoImg} alt="logo" />
+                
+                   <h2>Criar uma nova sala</h2>
+                    <form>
+                        <input 
+                        type="text"
+                        placeholder="nome da sala" 
+                        />
+                        <Button type="submit">
+                        Criar sala
+                        </Button>
+                    </form>
+                    
+                </div>
+            </main>
+        </div>
+    )
+}
